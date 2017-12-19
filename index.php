b3aeaa5e6af42c000696c5bc00f4af115922524c
alonj <alonj@campus.technion.ac.il>
2017-12-19 15:21:57 +0200

    new homepage, added pages for singular/file upload of data, php still does not display data

--- index.php
+++ index.php
@@ -17,16 +17,18 @@
         echo "error";
         die(print_r(sqlsrv_errors(), true));
     }
-      $sql = "SELECT name, (10*comments + 0.1*views)/duration as score  
+      $sql = "SELECT name, (10*comments + 0.1*views)/duration as score
               FROM Ted";
+//      $sql = "SELECT * FROM Ted";
       $result = sqlsrv_query($conn, $sql);
+      sqlsrv_close($conn);
       echo "<table border=1>";
       echo "<th><td>Lecture Name</td><td>Score</td></th>";
       while($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC))
       {
          echo "<tr><td>".$row['name'] . "</td><td>" . $row['score']."</td></tr>";
       }
-      echo "<table>"
+      echo "</table>";
     ?>
   </body>
 </html>
