3702512bb75b2712c49bfefca7c2f440e6515150
alonj <alonj@campus.technion.ac.il>
2017-12-20 15:30:04 +0200

    production

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
           <li><a href="singlelec.php">Add lecture (singular form)</a><br></li>
       </ul>
   </div>
-
+  <div class="middle">
   <?php
     $server = "tcp:techniondbcourse01.database.windows.net,1433";
     $c = array("Database" => "alonj", "UID" => "alonj", "PWD" => "Qwerty12!");
@@ -40,7 +40,7 @@
       }
       echo "</table>";
     ?>
-  <div class="middle"></div>
+  </div>
   <div class="right"></div>
   </body>
 </html>
