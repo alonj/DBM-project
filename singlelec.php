2fc798d1ef4ce5b9026fab10b50927722085b1e7
alonj <alonj@campus.technion.ac.il>
2017-12-20 16:30:07 +0200

    production

--- singlelec.php
+++ singlelec.php
@@ -54,42 +54,43 @@
         </p>
     </form>
 <?php
-//Get form data from singlelec.html
-$name = htmlspecialchars($_POST['name']);
-$main_speaker = htmlspecialchars($_POST['main_speaker']);
-$description = htmlspecialchars($_POST['description']);
-$event = htmlspecialchars($_POST['event']);
-$languages = htmlspecialchars($_POST['languages']);
-$speaker_occupation = htmlspecialchars($_POST['speaker_occupation']);
-$url = htmlspecialchars($_POST['url']);
-$duration = htmlspecialchars($_POST['duration']);
-$comments = htmlspecialchars($_POST['comments']);
-$views = htmlspecialchars($_POST['views']);
+if (isset($_POST["submit"])) {
+    //Get form data from singlelec.html
+    $name = htmlspecialchars($_POST['name']);
+    $main_speaker = htmlspecialchars($_POST['main_speaker']);
+    $description = htmlspecialchars($_POST['description']);
+    $event = htmlspecialchars($_POST['event']);
+    $languages = htmlspecialchars($_POST['languages']);
+    $speaker_occupation = htmlspecialchars($_POST['speaker_occupation']);
+    $url = htmlspecialchars($_POST['url']);
+    $duration = htmlspecialchars($_POST['duration']);
+    $comments = htmlspecialchars($_POST['comments']);
+    $views = htmlspecialchars($_POST['views']);
 
-//Connect to SQL server
-$server = "tcp:techniondbcourse01.database.windows.net,1433";
-$c = array("Database" => "alonj", "UID" => "alonj", "PWD" => "Qwerty12!");
-sqlsrv_configure('WarningsReturnAsErrors', 0);
-$conn = sqlsrv_connect($server, $c);
-if($conn === false)
-{
-    echo "error";
-    die(print_r(sqlsrv_errors(), true));
+    //Connect to SQL server
+    $server = "tcp:techniondbcourse01.database.windows.net,1433";
+    $c = array("Database" => "alonj", "UID" => "alonj", "PWD" => "Qwerty12!");
+    sqlsrv_configure('WarningsReturnAsErrors', 0);
+    $conn = sqlsrv_connect($server, $c);
+    if ($conn === false) {
+        echo "error";
+        die(print_r(sqlsrv_errors(), true));
+    }
+
+    //Insert form data into "Ted" table
+    $sql = "INSERT INTO Ted(name,  main_speaker,  description,  event,  languages,  speaker_occupation,  url,  duration, comments, views)
+                    VALUES('" . $name . "', 
+                           '" . $main_speaker . "',
+                           '" . $description . "',
+                           '" . $event . "',
+                           '" . $languages . "',
+                           '" . $speaker_occupation . "',
+                           '" . $url . "',
+                           '" . $duration . "',
+                           '" . $comments . "',
+                           '" . $views . "');";
+    $result = sqlsrv_query($conn, $sql);
 }
-
-//Insert form data into "Ted" table
-$sql = "INSERT INTO Ted(name,  main_speaker,  description,  event,  languages,  speaker_occupation,  url,  duration, comments, views)
-                VALUES('".$name."', 
-                       '".$main_speaker."',
-                       '".$description."',
-                       '".$event."',
-                       '".$languages."',
-                       '".$speaker_occupation."',
-                       '".$url."',
-                       '".$duration."',
-                       '".$comments."',
-                       '".$views."');";
-$result = sqlsrv_query($conn, $sql);
 ?>
 <!--</div>-->
 <div class="right"></div>
