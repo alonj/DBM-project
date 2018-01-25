bb5159aff9d256d2913009b67bf760501141c23a
alonj <alonj@campus.technion.ac.il>
2018-01-25 20:10:50 +0200

    heatmap attempt 1

--- heatmap.php
+++ heatmap.php
@@ -1,4 +1,4 @@
-<?php /*session_start(); */?><!--
+<?php session_start(); ?>
 
 <!DOCTYPE html>
 <html>
@@ -7,7 +7,19 @@
 </head>
 <body>
 <link rel="stylesheet" href='styles.css' type="text/css">
-<form action="<?php /*echo $_SERVER['PHP_SELF'];*/?>" method="post" style="width: 100%; background-color: white; float: left; min-height: 100%">
+<div class="topbanner">
+    <img height="61px" width="100px" src="https://static-assets.ny.gov/sites/all/themes/ny_gov/images/logo_footer.png" class="title" alt="New York - Public Transportation Information">
+    <p class="quote">Public Transportation Information</p>
+</div>
+<div class="left">
+    <ul>
+        <li><a href="homepage.php" target="center_frame">Homepage</a><br></li>
+        <li><a href="singleform.php" target="center_frame">Add driver (singular form)</a><br></li>
+        <li><a href="fileform.php" target="center_frame">Add ride details from file<br>(.csv format)</a><br></li>
+        <li><a href="heatmap.php"<!--target="center_frame"-->>New York Heatmap</a><br></li>
+    </ul>
+</div>
+<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post" style="width: 100%; background-color: white; float: left; min-height: 100%">
     <div style="float: left; padding-left: 20%; padding-top: 5%; font-family: Calibri">
         Hour:<br>
         <input type="number" title="hour" name="hour" min="1" max="24" required autofocus><br><br>
@@ -26,7 +38,7 @@
     </div>
 </form>
 <?php
-/*$latitude = 40.7128;
+$latitude = 40.7128;
 $longitude = -74.0060;
 sqlsrv_configure('WarningsReturnAsErrors', 0);
 $conn = sqlsrv_connect($_SESSION["server"], $_SESSION["c"]);
@@ -63,14 +75,14 @@ if (isset($_POST["submit"])) {
     elseif ($heat > 50){
         $color = "#d65562";
     }
-*/?>
+?>
 <div id="map_div">
     <script>
         function myMap() {
-            var lat = <?php /*echo json_encode($latitude); */?>;
-            var lon = <?php /*echo json_encode($longitude); */?>;
-            var rad = <?php /*echo json_encode($radius_km); */?>;
-            var color =<?php /*echo json_encode("#ff948e"); */?>;
+            var lat = <?php echo json_encode($latitude); ?>;
+            var lon = <?php echo json_encode($longitude); ?>;
+            var rad = <?php echo json_encode($radius_km); ?>;
+            var color =<?php echo json_encode("#ff948e"); ?>;
             var position = new google.maps.LatLng(lat, lon);
             var mapProp = {
                 center:position,
@@ -90,138 +102,6 @@ if (isset($_POST["submit"])) {
         }
     </script>
 </div>
-<?php /*echo '<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC_ml_vTDIuJm62aNLcPfmXgbOhTxGb7KE&callback=myMap"></script>';}*/?>
-</body>
-</html>-->
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <link rel="stylesheet" href="style.css">
-    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    <title>UBER-NYC Heatmap</title>
-</head>
-<body>
-<?php
-// Connecting to the database
-$server = "tcp:techniondbcourse01.database.windows.net,1433";
-$user = "dbstudents";
-$pass = "Qwerty12!";
-$database = "dbstudents";
-$c = array("Database" => $database, "UID" => $user, "PWD" => $pass);
-sqlsrv_configure('WarningsReturnAsErrors', 0);
-$conn = sqlsrv_connect($server, $c);
-if ($conn === false) {
-    echo "error";
-    die(print_r(sqlsrv_errors(), true));
-}
-//echo "connected to DB<br>"; //debug
-?>
-<form class="card" action="<?php $_SERVER['PHP_SELF'] ?>" method="POST" style="width: 50rem">
-    <h1>Data visualization:</h1>
-    <table border="0">
-        <tr>
-            <td>
-                <select name="hour" required>
-                    <option value="" disabled selected hidden>Hour</option>
-                    <?php
-                    foreach (range(0, 23) as $number) {
-                        echo '<option value=' . $number . '>' . $number . '</option>';
-                    }
-                    ?>
-                </select>
-            </td>
-            <td><label>
-                    <input name="lat" type="number" size="25" step="any" required
-                           min="-90" max="90" title="Latitude is a required field!"
-                           placeholder="Latitude"
-                    />
-                </label></td>
-            <td><label>
-                    <input name="long" type="number" size="25" step="any" required
-                           min="-180" max="180" title="Longitude is a required field!"
-                           placeholder="Longitude"
-                    />
-                </label></td>
-            <td><label>
-                    <input name="radius" type="number" size="25" step="any" required
-                           min="0" title="Radius is a required field!"
-                           placeholder="Radius"
-                    />
-                </label></td>
-        </tr>
-        <tr>
-            <td colspan="4">
-                <button class="btn" type="submit" name="submit" value="submit" onclick="">Submit</button>
-            </td>
-        </tr>
-    </table>
-</form>
-<?php
-if (isset($_POST["submit"])) {
-$hour = $_POST['hour'];
-$pLat = $_POST['lat'];
-$pLong = $_POST['long'];
-$radius = $_POST['radius'];
-$sql = "SELECT count(car_id) AS carCnt
-                        FROM small_drive Details
-                        WHERE (datepart(HOUR, Details.Ctime) = ".$hour.") AND
-                              ((1609.34 * 2 * 3961 * asin(sqrt(POWER((sin(radians((" . $pLat . " - Details.location_lat) / 2))) , 2) +
-                                           cos(radians(Details.location_lat)) * cos(radians(" . $pLat . ")) *
-                                           POWER((sin(radians((" . $pLong . " - Details.location_long) / 2))) , 2)))) <= " . $radius . ");";
-//    echo $sql . "<br>"; //debug
-$result = sqlsrv_query($conn, $sql);
-// In case of failure
-if (!$result) {
-    die("Couldn't add the part specified.<br>");
-}
-$row = sqlsrv_fetch_array($result,SQLSRV_FETCH_ASSOC);
-$cnt = $row['carCnt'];
-if ($cnt==0){
-    $color = "#b0bec5";
-}
-elseif ($cnt <= 20) {
-    $color = "#1976d2";
-} elseif ($cnt <= 50) {
-    $color = "#f50057";
-} else {
-    $color = "#f44336";
-}
-?>
-<div>
-    <div id="googleMap">
-        <script>
-            function myMap() {
-                var qLat =  <?php echo json_encode($pLat,JSON_NUMERIC_CHECK); ?>;
-                var qLng = <?php echo json_encode($pLong,JSON_NUMERIC_CHECK); ?>;
-                var qRadius = <?php echo json_encode($radius,JSON_NUMERIC_CHECK); ?>;
-                var qColor = <?php echo json_encode($color); ?>;
-                var qPos = new google.maps.LatLng(qLat, qLng);
-                var mapProp = {
-                    center: qPos,
-                    zoom: 10
-                };
-                var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-                var qCircle = new google.maps.Circle({
-                    center: qPos,
-                    radius: qRadius,
-                    strokeColor: qColor,
-                    strokeOpacity: 0.8,
-                    strokeWeight: 2,
-                    fillColor: qColor,
-                    fillOpacity: 0.4
-                });
-                qCircle.setMap(map);
-                map.fitBounds(qCircle.getBounds());//fits the maps bounds to circle
-                document.getElementById('googleMap').scrollIntoView();
-            }
-        </script>
-    </div>
-    <?php
-    echo '<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCv6wuQJDE4QzG9Oy_FDXcOtuptY4Lksu8&callback=myMap"></script>';
-    }//closure of isset
-    ?>
-</div>
+<?php echo '<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC_ml_vTDIuJm62aNLcPfmXgbOhTxGb7KE&callback=myMap"></script>';}?>
 </body>
 </html>
